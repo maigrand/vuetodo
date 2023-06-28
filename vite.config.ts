2af72fe278e8ecf30e470caa8cfec3e2b0980ecf
Mai Grand <mkmaxz48@gmail.com>
2023-06-28 17:46:05 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue2'
+
+export default defineConfig({
+    plugins: [vue()],
+    server: {
+        open: true,
+        port: 3000,
+        strictPort: true,
+    },
+});
